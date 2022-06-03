68919591f7e58ee6a1d97bce219d7221c2901f0e
João Vitor <joaovitorfischborn@icloud.com>
2022-06-03 15:33:02 -0300

    Requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,15 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function')
+  })
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', () => {
+    expect(fetchProducts('computador')).toBe(fetchProducts())
+  })
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint', async () => {
+    const actual = await fetchProducts('computador')
+    const expected = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    expect(actual).toBe(expected)
+  })
 });
