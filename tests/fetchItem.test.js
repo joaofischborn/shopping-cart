d3ff263b071491a0fc612155df0675f6eab4d385
João Vitor <joaovitorfischborn@icloud.com>
2022-06-03 17:41:28 -0300

    Requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,10 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function')
+  })
+  it('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', () => {
+    expect(fetchItem("MLB1615760527")).toBe(fetchItem())
+  })
 });
